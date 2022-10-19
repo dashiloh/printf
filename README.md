dc98c299984014e7b6ac0143d4e50304e3ba3eef
Dashiloh <princeshiloh@yahoo.com>
2022-10-19 00:06:00 +0000

    PROJECT GROUP

---
+++ README.md
@@ -0,0 +1,2 @@
+# c-printf
+_printf is a function to do formatted printing. In this program we develop the function for simple printing and the format specifications printing.
---
+++ test-files/hex.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * hex: hex-decimal value
+ * Return: 0.
+ */
+
+
+int main(void)
+{
+	_printf("%x\n", 45);
+	_printf("%x\n", 4512);
+	_printf("%X\n", 45);
+	_printf("%X\n", 4512);
+	return (0);
+}
